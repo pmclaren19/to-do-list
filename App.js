0dc5a19d88f9924e9e96a6c5fc2a2a63e45470e1
Peri Mclaren <perimclaren@Peris-MacBook-Pro.local>
2025-03-23 14:59:57 -0600

    First commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { ToDoWrapper } from "./components/ToDoWrapper";
+function App() {
+  return (
+    <div className="App">
+      <ToDoWrapper />
+    </div>
+  );
+}
+
+export default App;
